31ad2d3d2fd228c5191359ebfe1c67bb1632d4d9
Jordan Milne <jmilne@uber.com>
2016-10-13 15:30:22 -0700

    Don't replace regex / function placeholders within string literals
    Previously we weren't checking if the quote that started the placeholder
    was escaped or not, meaning an object like
    
        {"foo": /1"/, "bar": "a\"@__R-<UID>-0__@"}
    
    Would be serialized as
    
        {"foo": /1"/, "bar": "a\/1"/}
    
    meaning an attacker could escape out of `bar` if they controlled both
    `foo` and `bar` and were able to guess the value of `<UID>`.
    
    UID was generated once on startup, was chosen using `Math.random()` and
    had a keyspace of roughly 4 billion, so within the realm of an online
    attack.
    
    Here's a simple example that will cause `console.log()` to be called when
    the `serialize()`d version is `eval()`d
    
        eval('('+ serialize({"foo": /1" + console.log(1)/i, "bar": '"@__R-<UID>-0__@'}) + ')');
    
    Where `<UID>` is the guessed `UID`.
    
    This fixes the issue by ensuring that placeholders are not preceded by
    a backslash.
    
    We also switch to a higher entropy `UID` to prevent people from guessing it.

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ See the accompanying LICENSE file for terms.
 
 'use strict';
 
-var isRegExp = require('util').isRegExp;
+var isRegExp    = require('util').isRegExp;
+var randomBytes = require('randombytes');
 
 // Generate an internal UID to make the regexp pattern harder to guess.
-var UID                 = Math.floor(Math.random() * 0x10000000000).toString(16);
-var PLACE_HOLDER_REGEXP = new RegExp('"@__(F|R)-' + UID + '-(\\d+)__@"', 'g');
+var UID_LENGTH          = 16;
+var UID                 = generateUID();
+var PLACE_HOLDER_REGEXP = new RegExp('(\\\\)?"@__(F|R)-' + UID + '-(\\d+)__@"', 'g');
 
 var IS_NATIVE_CODE_REGEXP = /\{\s*\[native code\]\s*\}/g;
 var UNSAFE_CHARS_REGEXP   = /[<>\/\u2028\u2029]/g;
@@ -27,6 +29,15 @@ var ESCAPED_CHARS = {
 
 function escapeUnsafeChars(unsafeChar) {
     return ESCAPED_CHARS[unsafeChar];
+}
+
+function generateUID() {
+    var bytes = randomBytes(UID_LENGTH);
+    var result = '';
+    for(var i=0; i<UID_LENGTH; ++i) {
+        result += bytes[i].toString(16);
+    }
+    return result;
 }
 
 module.exports = function serialize(obj, options) {
@@ -92,7 +103,13 @@ module.exports = function serialize(obj, options) {
     // Replaces all occurrences of function and regexp placeholders in the JSON
     // string with their string representations. If the original value can not
     // be found, then `undefined` is used.
-    return str.replace(PLACE_HOLDER_REGEXP, function (match, type, valueIndex) {
+    return str.replace(PLACE_HOLDER_REGEXP, function (match, backSlash, type, valueIndex) {
+        // The placeholder may not be preceded by a backslash. This is to prevent
+        // replacing things like `"a\"@__R-<UID>-0__@"` and thus outputting
+        // invalid JS.
+        if (backSlash) {
+            return match;
+        }
         if (type === 'R') {
             return regexps[valueIndex].toString();
         }
