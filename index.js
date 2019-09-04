4dfd9e5b64027663d82d398823f5948b2a2e56d7
Ryuichi Okumura <okuryu@okuryu.com>
2019-09-04 21:05:52 +0900

    Revert "support serialize undefined (#54)"
    This reverts commit 356987fa6f72f747b3abd0e6f7318a800a5fb7ff.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ See the accompanying LICENSE file for terms.
 
 // Generate an internal UID to make the regexp pattern harder to guess.
 var UID                 = Math.floor(Math.random() * 0x10000000000).toString(16);
-var PLACE_HOLDER_REGEXP = new RegExp('"@__(F|R|D|M|S|U)-' + UID + '-(\\d+)__@"', 'g');
+var PLACE_HOLDER_REGEXP = new RegExp('"@__(F|R|D|M|S)-' + UID + '-(\\d+)__@"', 'g');
 
 var IS_NATIVE_CODE_REGEXP = /\{\s*\[native code\]\s*\}/g;
 var IS_PURE_FUNCTION = /function.*?\(/;
@@ -44,13 +44,11 @@ module.exports = function serialize(obj, options) {
     var dates     = [];
     var maps      = [];
     var sets      = [];
-    var undefs    = [];
 
     // Returns placeholders for functions and regexps (identified by index)
     // which are later replaced by their string representation.
     function replacer(key, value) {
-
-        if (!value && value !== undefined) {
+        if (!value) {
             return value;
         }
 
@@ -79,10 +77,6 @@ module.exports = function serialize(obj, options) {
 
         if (type === 'function') {
             return '@__F-' + UID + '-' + (functions.push(origValue) - 1) + '__@';
-        }
-
-        if (type === 'undefined') {
-            return '@__U-' + UID + '-' + (undefs.push(origValue) - 1) + '__@';
         }
 
         return value;
@@ -125,12 +119,6 @@ module.exports = function serialize(obj, options) {
       return serializedFn;
     }
 
-    // Protects against `JSON.stringify()` returning `undefined`, by serializing
-    // to the literal string: "undefined".
-    if (obj === undefined) {
-        return String(obj);
-    }
-
     var str;
 
     // Creates a JSON string representation of the value.
@@ -154,7 +142,7 @@ module.exports = function serialize(obj, options) {
         str = str.replace(UNSAFE_CHARS_REGEXP, escapeUnsafeChars);
     }
 
-    if (functions.length === 0 && regexps.length === 0 && dates.length === 0 && maps.length === 0 && sets.length === 0 && undefs.length === 0) {
+    if (functions.length === 0 && regexps.length === 0 && dates.length === 0 && maps.length === 0 && sets.length === 0) {
         return str;
     }
 
@@ -176,10 +164,6 @@ module.exports = function serialize(obj, options) {
 
         if (type === 'S') {
             return "new Set(" + serialize(Array.from(sets[valueIndex].values()), options) + ")";
-        }
-
-        if (type === 'U') {
-            return 'undefined'
         }
 
         var fn = functions[valueIndex];
